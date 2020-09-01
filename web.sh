d062048cf786be47351658c2e345aa9b871c0add
Corvin Tiperciuc <cuvidk@gmail.com>
2020-09-01 22:47:10 +0300

    Created a script which installs / updates web hacking tools

---
+++ web.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+PACKAGES="go\
+         "
+GO_PACKAGE_PATH="/opt/go"
+
+if [ `id -u` -ne 0 ]; then
+    echo "Usage: sudo $0"
+    exit 1
+fi
+
+install_golang() {
+    pacman -S --needed --noconfirm go
+    mkdir -p "$GO_PACKAGE_PATH"
+    export GOPATH="$GO_PACKAGE_PATH"
+    echo "export GOPATH=$GO_PACKAGE_PATH" >"/etc/profile.d/go.sh"
+}
+
+install_assetfinder() {
+    go get -u github.com/tomnomnom/assetfinder
+    ln -s "$GO_PACKAGE_PATH/bin/assetfinder" /usr/bin/assetfinder
+}
+
+install_golang
+install_assetfinder
