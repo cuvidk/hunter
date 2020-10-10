213bea37cde0c056fcc69ef6ea6366600e8b7f19
Corvin Tiperciuc <cuvidk@gmail.com>
2020-10-10 17:08:45 +0300

    Fixed output redirection when no tty available
    * Added a special function in util.sh that will redirect unintented
      output to a log file and intended output to the inherited output
      stream. Before this change, the intended output was redirected to the
      connected tty, however in case none was availabe (e.g. docker build
              environment) it was redirecting it to a file called "not a
      tty". This behavior was generated by the redirection to $(tty).

--- prerequisites.sh
+++ prerequisites.sh
@@ -4,12 +4,12 @@
 
 install_package() {
     local package_name=$1
-    pacman -S --noconfirm "${package_name}"
+    pacman -S --noconfirm --needed "${package_name}"
 }
 
 install_packages() {
     for package in ${PACKAGES}; do
-        perform_task_arg install_package ${package} "Installing package ${package} "
+        perform_task_arg install_package ${package} "Installing package ${package}"
     done
 }
 
@@ -20,13 +20,12 @@ install_config_files() {
 #########################################################################
 
 WORKING_DIR="$(realpath "$(dirname "${0}")")"
-
 . "${WORKING_DIR}/util.sh"
 
-STDOUT_LOG="${WORKING_DIR}/stdout.log"
-STDERR_LOG="${WORKING_DIR}/stderr.log"
+setup_output
 
 PACKAGES="vim
+          sudo
           man-db
           man-pages
           texinfo
@@ -34,17 +33,9 @@ PACKAGES="vim
 	  base-devel
           git
           zsh
-          sudo
           "
-
-if [ -t 1 ]; then
-    "${0}" "${@}" >"${STDOUT_LOG}" 2>${STDERR_LOG}
-    exit 0
-fi
 
 perform_task install_packages
 perform_task install_config_files
 
-errors_encountered &&
-    print_msg "ERR: ${0} finished with errors. Check ${STDERR_LOG} / ${STDOUT_LOG} for details.\n" ||
-    print_msg "${0} finished with success.\n"
+check_for_errors
--- util.sh
+++ util.sh
@@ -2,15 +2,28 @@
 
 g_err_flag=0
 
+setup_output() {
+    WORKING_DIR="$(realpath "$(dirname "${0}")")"
+    OUT_REDIRECTED="${WORKING_DIR}/${0}.log"
+    [ -e /proc/$$/fd/1 ] && OUT="$(realpath /proc/$$/fd/1)"
+    exec 1>"${OUT_REDIRECTED}" 2>&1
+}
+
 print_msg() {
-    echo -n -e "$1">>$(tty)
+    [ -n "${OUT}" ] &&
+        echo -n -e "$1">>${OUT} ||
+        echo -n -e "$1"
 }
 
 perform_task() {
     local task=$1
     local message=$2
     [ -n "${message}" ] && print_msg "${message}\r"
+    echo "#################################################"
+    echo "                   ${message}                    "
+    echo "#################################################"
     ${task}
+    echo "#################################################"
     local ret=$?
     if [ ${ret} -eq 0 ]; then
         [ -n "${message}" ] && print_msg "[ OK ] ${message}\n"
@@ -26,7 +39,11 @@ perform_task_arg() {
     local arg=$2
     local message=$3
     [ -n "${message}" ] && print_msg "${message}\r"
+    echo "#################################################"
+    echo "                   ${message}                    "
+    echo "#################################################"
     ${task} ${arg}
+    echo "#################################################"
     local ret=$?
     if [ ${ret} -eq 0 ]; then
         [ -n "${message}" ] && print_msg "[ OK ] ${message}\n"
@@ -37,6 +54,8 @@ perform_task_arg() {
     return ${ret}
 }
 
-errors_encountered() {
-    [ ${g_err_flag} -eq 1 ]
+check_for_errors() {
+    [ ${g_err_flag} -eq 1 ] &&
+        print_msg "[ WARNING ]: ${0} finished with errors. See ${OUT_REDIRECTED} for details.\n" ||
+        print_msg "[ SUCCESS ]: ${0} finished with success."
 }
