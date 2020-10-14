b9dbe8409fcc11585eb88b6ddf8c1d3511b6a77c
Corvin Tiperciuc <cuvidk@gmail.com>
2020-10-14 11:31:57 +0300

    A bit of code cleanup

--- tools.sh
+++ tools.sh
@@ -18,34 +18,35 @@ install_metabigor() {
 install_asnlookup() {
     git clone "https://github.com/yassineaboukir/Asnlookup" "${WORKING_DIR}/Asnlookup"
     pip install -r "${WORKING_DIR}/Asnlookup/requirements.txt"
-    mkdir /opt/asnlookup
-    cp "${WORKING_DIR}/Asnlookup/asnlookup.py" /opt/asnlookup/
-    chmod +x /opt/asnlookup/asnlookup.py
-    cp "${WORKING_DIR}/Asnlookup/config.py" /opt/asnlookup/
-    chown -R "${g_user}:${g_user}" /opt/asnlookup
-    ln -s /opt/asnlookup/asnlookup.py /usr/bin/asnlookup
+    mkdir "${ASNLOOKUP_PATH}"
+    cp "${WORKING_DIR}/Asnlookup/asnlookup.py" "${ASNLOOKUP_PATH}"
+    chmod +x "${ASNLOOKUP_PATH}/asnlookup.py"
+    cp "${WORKING_DIR}/Asnlookup/config.py" "${ASNLOOKUP_PATH}"
+    chown -R "${g_user}:${g_user}" "${ASNLOOKUP_PATH}"
+    ln -s "${ASNLOOKUP_PATH}/asnlookup.py" /usr/bin/asnlookup
     rm -rf "${WORKING_DIR}/Asnlookup"
 }
 
 install_domlink() {
     git clone "https://github.com/vysecurity/DomLink.git" "${WORKING_DIR}/DomLink"
     pip install -r "${WORKING_DIR}/DomLink/requirements.txt"
-    mkdir /opt/domlink
-    cp "${WORKING_DIR}/DomLink/domLink.py" /opt/domlink
-    chmod +x /opt/domlink/domLink.py
-    mv "${WORKING_DIR}/DomLink/domLink.cfg.example" /opt/domlink/domLink.cfg
-    chown -R "${g_user}:${g_user}" /opt/domlink
-    ln -s /opt/domlink/domLink.py /usr/bin/domlink
+    mkdir "${DOMLINK_PATH}"
+    cp "${WORKING_DIR}/DomLink/domLink.py" "${DOMLINK_PATH}"
+    chmod +x "${DOMLINK_PATH}/domLink.py"
+    # API KEY
+    mv "${WORKING_DIR}/DomLink/domLink.cfg.example" "${DOMLINK_PATH}/domLink.cfg"
+    chown -R "${g_user}:${g_user}" "${DOMLINK_PATH}"
+    ln -s "${DOMLINK_PATH}/domLink.py" /usr/bin/domlink
     rm -rf "${WORKING_DIR}/DomLink"
 }
 
 install_getrelationship() {
     wget "https://raw.githubusercontent.com/m4ll0k/Bug-Bounty-Toolz/master/getrelationship.py"
-    mkdir /opt/getrelationship
-    mv getrelationship.py /opt/getrelationship/getrelationship.py
-    chmod +x /opt/getrelationship/getrelationship.py
-    chown -R "${g_user}:${g_user}" /opt/getrelationship
-    ln -s /opt/getrelationship/getrelationship.py /usr/bin/getrelationship
+    mkdir "${GETRELATIONSHIP_PATH}"
+    mv getrelationship.py "${GETRELATIONSHIP_PATH}/getrelationship.py"
+    chmod +x "${GETRELATIONSHIP_PATH}/getrelationship.py"
+    chown -R "${g_user}:${g_user}" "${GETRELATIONSHIP_PATH}"
+    ln -s "${GETRELATIONSHIP_PATH}/getrelationship.py" /usr/bin/getrelationship
 }
 
 install_gospider() {
@@ -61,11 +62,11 @@ install_hakrawler() {
 install_subdomainizer() {
     git clone "https://github.com/nsonaniya2010/SubDomainizer.git" "${WORKING_DIR}/SubDomainizer"
     pip install -r "${WORKING_DIR}/SubDomainizer/requirements.txt"
-    mkdir /opt/subdomainizer
-    cp "${WORKING_DIR}/SubDomainizer/SubDomainizer.py" /opt/subdomainizer/
-    chmod +x /opt/subdomainizer/SubDomainizer.py
-    chown -R "${g_user}:${g_user}" /opt/subdomainizer
-    ln -s /opt/subdomainizer/SubDomainizer.py /usr/bin/subdomainizer
+    mkdir "${SUBDOMAINIZER_PATH}"
+    cp "${WORKING_DIR}/SubDomainizer/SubDomainizer.py" "${SUBDOMAINIZER_PATH}"
+    chmod +x "${SUBDOMAINIZER_PATH}/SubDomainizer.py"
+    chown -R "${g_user}:${g_user}" "${SUBDOMAINIZER_PATH}"
+    ln -s "${SUBDOMAINIZER_PATH}/SubDomainizer.py" /usr/bin/subdomainizer
     rm -rf "${WORKING_DIR}/SubDomainizer"
 }
 
@@ -74,24 +75,24 @@ install_subscraper() {
     cd "${WORKING_DIR}/subscraper"
     python3 setup.py install
     pip install ipparser
-    mkdir /opt/subscraper
-    mv /usr/bin/subscraper /opt/subscraper/subscraper
-    echo '#!/bin/sh' >/opt/subscraper/subscraper.sh
-    echo "/opt/subscraper/subscraper --censys-api ${CENSYS_API_KEY} --censys-secret ${CENSYS_SECRET}" '${@}' >>/opt/subscraper/subscraper.sh
-    chmod +x /opt/subscraper/subscraper.sh
-    chown -R "${g_user}:${g_user}" /opt/subscraper
-    ln -s /opt/subscraper/subscraper.sh /usr/bin/subscraper
+    mkdir "${SUBSCRAPER_PATH}"
+    mv /usr/bin/subscraper "${SUBSCRAPER_PATH}/subscraper"
+    echo '#!/bin/sh' >"${SUBSCRAPER_PATH}/subscraper.sh"
+    echo "${SUBSCRAPER_PATH}/subscraper --censys-api ${CENSYS_API_KEY} --censys-secret ${CENSYS_SECRET}" '${@}' >>"${SUBSCRAPER_PATH}/subscraper.sh"
+    chmod +x "${SUBSCRAPER_PATH}/subscraper.sh"
+    chown -R "${g_user}:${g_user}" "${SUBSCRAPER_PATH}"
+    ln -s "${SUBSCRAPER_PATH}/subscraper.sh" /usr/bin/subscraper
     cd -
     rm -rf "${WORKING_DIR}/subscraper"
 }
 
 install_shosubgo() {
-    git clone "https://github.com/incogbyte/shosubgo" /opt/shosubgo
-    echo '#!/bin/sh' >/opt/shosubgo/shosubgo.sh
-    echo "go run /opt/shosubgo/main.go -s ${SHODAN_API_KEY}" '${@}' >>/opt/shosubgo/shosubgo.sh
-    chmod +x /opt/shosubgo/shosubgo.sh
-    ln -s /opt/shosubgo/shosubgo.sh /usr/bin/shosubgo
-    chown -R "${g_user}:${g_user}" /opt/shosubgo
+    git clone "https://github.com/incogbyte/shosubgo" "${SHOSUBGO_PATH}"
+    echo '#!/bin/sh' >"${SHOSUBGO_PATH}/shosubgo.sh"
+    echo "go run ${SHOSUBGO_PATH}/main.go -s ${SHODAN_API_KEY}" '${@}' >>"${SHOSUBGO_PATH}/shosubgo.sh"
+    chmod +x "${SHOSUBGO_PATH}/shosubgo.sh"
+    ln -s "${SHOSUBGO_PATH}/shosubgo.sh" /usr/bin/shosubgo
+    chown -R "${g_user}:${g_user}" "${SHOSUBGO_PATH}"
 }
 
 install_github_subdomains() {
@@ -100,22 +101,22 @@ install_github_subdomains() {
 }
 
 install_github_search() {
-    git clone "https://github.com/gwen001/github-search.git" /opt/github-search
-    chown -R "${g_user}:${g_user}" /opt/github-search
+    git clone "https://github.com/gwen001/github-search.git" "${GITHUB_SEARCH_PATH}"
+    chown -R "${g_user}:${g_user}" "${GITHUB_SEARCH_PATH}"
 }
 
 install_assetfinder() {
     export GO111MODULE=on
     sudo -H -E -u "${g_user}" go get -u github.com/tomnomnom/assetfinder
     unset GO111MODULE
-    ln -s "$GO_PACKAGE_PATH/bin/assetfinder" /usr/bin/assetfinder
+    ln -s "${GO_PACKAGE_PATH}/bin/assetfinder" /usr/bin/assetfinder
 }
 
 install_httprobe() {
     export GO111MODULE=on
     sudo -H -E -u "${g_user}" go get -u github.com/tomnomnom/httprobe
     unset GO111MODULE
-    ln -s "$GO_PACKAGE_PATH/bin/httprobe" /usr/bin/httprobe
+    ln -s "${GO_PACKAGE_PATH}/bin/httprobe" /usr/bin/httprobe
 }
 
 install_amass() {
@@ -124,8 +125,8 @@ install_amass() {
     unset GO111MODULE
     ln -s "${GO_PACKAGE_PATH}/bin/amass" /usr/bin/amass
     git clone "https://github.com/OWASP/Amass" "${WORKING_DIR}/Amass"
-    mkdir -p /opt/wordlists/amass
-    cp "${WORKING_DIR}/Amass/examples/wordlists/*" /opt/wordlists/amass
+    mkdir -p "${WORDLISTS_PATH}/amass"
+    cp "${WORKING_DIR}/Amass/examples/wordlists/*" "${WORDLISTS_PATH}/amass"
     rm -rf "${WORKING_DIR}/Amass"
 }
 
@@ -169,11 +170,11 @@ install_massdns() {
     git clone 'https://github.com/blechschmidt/massdns.git' "${WORKING_DIR}/massdns"
     cd "${WORKING_DIR}/massdns"
     make
-    mkdir /opt/massdns
-    cp "${WORKING_DIR}/massdns/bin/massdns" /opt/massdns/
-    cp -R "${WORKING_DIR}/massdns/lists" /opt/massdns/
-    chown -R "${g_user}:${g_user}" /opt/massdns
-    ln -s /opt/massdns/massdns /usr/bin/massdns
+    mkdir "${MASSDNS_PATH}"
+    cp "${WORKING_DIR}/massdns/bin/massdns" "${MASSDNS_PATH}"
+    cp -R "${WORKING_DIR}/massdns/lists" "${MASSDNS_PATH}"
+    chown -R "${g_user}:${g_user}" "${MASSDNS_PATH}"
+    ln -s "${MASSDNS_PATH}/massdns" /usr/bin/massdns
     cd -
     rm -rf "${WORKING_DIR}/massdns"
 }
@@ -182,20 +183,20 @@ install_masscan() {
     git clone 'https://github.com/robertdavidgraham/masscan' "${WORKING_DIR}/masscan"
     cd "${WORKING_DIR}/masscan"
     make
-    mkdir /opt/masscan
-    cp "${WORKING_DIR}/masscan/bin/masscan" /opt/masscan/
-    chown -R "${g_user}:${g_user}" /opt/masscan
-    ln -s /opt/masscan/masscan /usr/bin/masscan
+    mkdir "${MASSCAN_PATH}"
+    cp "${WORKING_DIR}/masscan/bin/masscan" "${MASSCAN_PATH}"
+    chown -R "${g_user}:${g_user}" "${MASSCAN_PATH}"
+    ln -s "${MASSCAN_PATH}/masscan" /usr/bin/masscan
     cd -
     rm -rf "${WORKING_DIR}/masscan"
 }
 
 install_dnmasscan() {
     git clone "https://github.com/rastating/dnmasscan.git" "${WORKING_DIR}/dnmasscan"
-    mkdir /opt/dnmasscan
-    cp "${WORKING_DIR}/dnmasscan/dnmasscan" /opt/dnmasscan/
-    chown -R "${g_user}:${g_user}" /opt/dnmasscan
-    ln -s /opt/dnmasscan/dnmasscan /usr/bin/dnmasscan
+    mkdir "${DNMASSCAN_PATH}"
+    cp "${WORKING_DIR}/dnmasscan/dnmasscan" "${DNMASSCAN_PATH}"
+    chown -R "${g_user}:${g_user}" "${DNMASSCAN_PATH}"
+    ln -s "${DNMASSCAN_PATH}/dnmasscan" /usr/bin/dnmasscan
     rm -rf "${WORKING_DIR}/dnmasscan"
 }
 
@@ -209,24 +210,24 @@ install_medusa() {
 
 install_brutespray() {
     git clone "https://github.com/x90skysn3k/brutespray.git" "${WORKING_DIR}/brutespray"
-    mkdir /opt/brutespray
-    mkdir -p /opt/wordlists/brutespray
+    mkdir "${BRUTESPRAY_PATH}"
+    mkdir -p "${WORDLISTS_PATH}/brutespray"
     pip install -r "${WORKING_DIR}/brutespray/requirements.txt"
-    cp "${WORKING_DIR}/brutespray/brutespray.py" /opt/brutespray/
-    chown -R "${g_user}:${g_user}" /opt/brutespray
-    ln -s /opt/brutespray/brutespray.py /usr/bin/brutespray
-    cp -R "${WORKING_DIR}/brutespray/wordlist/" /opt/wordlists/brutespray/
+    cp "${WORKING_DIR}/brutespray/brutespray.py" "${BRUTESPRAY_PATH}"
+    chown -R "${g_user}:${g_user}" "${BRUTESPRAY_PATH}"
+    ln -s "${BRUTESPRAY_PATH}/brutespray.py" /usr/bin/brutespray
+    cp -R "${WORKING_DIR}/brutespray/wordlist/" "${WORDLISTS_PATH}/brutespray/"
     rm -rf "${WORKING_DIR}/brutespray"
 }
 
 install_favfreak() {
     git clone "https://github.com/devanshbatham/FavFreak" "${WORKING_DIR}/FavFreak"
-    mkdir /opt/favfreak
+    mkdir "${FAVFREAK_PATH}"
     pip install -r "${WORKING_DIR}/FavFreak/requirements.txt"
-    cp "${WORKING_DIR}/FavFreak/favfreak.py" /opt/favfreak
-    chmod +x /opt/favfreak/favfreak.py
-    chown -R "${g_user}:${g_user}" /opt/favfreak
-    ln -s /opt/favfreak/favfreak.py /usr/bin/favfreak
+    cp "${WORKING_DIR}/FavFreak/favfreak.py" "${FAVFREAK_PATH}"
+    chmod +x "${FAVFREAK_PATH}/favfreak.py"
+    chown -R "${g_user}:${g_user}" "${FAVFREAK_PATH}"
+    ln -s "${FAVFREAK_PATH}/favfreak.py" /usr/bin/favfreak
     rm -rf "${WORKING_DIR}/FavFreak"
 }
 
@@ -236,7 +237,7 @@ install_subover() {
 }
 
 fix_wordlists_owner() {
-    chown -R "${g_user}:${g_user}" /opt/wordlists
+    chown -R "${g_user}:${g_user}" "${WORDLISTS_PATH}"
 }
 
 install_all() {
@@ -272,7 +273,7 @@ install_all() {
 
 remove_golang() {
     pacman -Rs --noconfirm go
-    rm -rf /opt/go
+    rm -rf "${GO_PACKAGE_PATH}"
     rm -rf /etc/profile.d/go.sh
 }
 
@@ -281,17 +282,17 @@ remove_metabigor() {
 }
 
 remove_asnlookup() {
-    rm -rf /opt/asnlookup
+    rm -rf "${ASNLOOKUP_PATH}"
     rm -rf /usr/bin/asnlookup
 }
 
 remove_domlink() {
-    rm -rf /opt/domlink
+    rm -rf "${DOMLINK_PATH}"
     rm -rf /usr/bin/domlink
 }
 
 remove_getrelationship() {
-    rm -rf /opt/getrelationship
+    rm -rf "${GETRELATIONSHIP_PATH}"
     rm -rf /usr/bin/getrelationship
 }
 
@@ -304,17 +305,17 @@ remove_hakrawler() {
 }
 
 remove_subdomainizer() {
-    rm -rf /opt/subdomainizer
+    rm -rf "${SUBDOMAINIZER_PATH}"
     rm -rf /usr/bin/subdomainizer
 }
 
 remove_subscraper() {
-    rm -rf /opt/subscraper
+    rm -rf "${SUBSCRAPER_PATH}"
     rm -rf /usr/bin/subscraper
 }
 
 remove_shosubgo() {
-    rm -rf /opt/shosubgo
+    rm -rf "${SHOSUBGO_PATH}"
     rm -rf /usr/bin/shosubgo
 }
 
@@ -323,7 +324,7 @@ remove_github_subdomains() {
 }
 
 remove_github_search() {
-    rm -rf /opt/github-search
+    rm -rf "${GITHUB_SEARCH_PATH}"
 }
 
 remove_assetfinder() {
@@ -339,7 +340,7 @@ remove_amass() {
 }
 
 remove_eyewitness() {
-    rm -rf /opt/eyewitness
+    rm -rf "${EYEWITNESS_PATH}"
     rm -rf /usr/bin/eyewitness
     # eyewitness has a custom installer that installs
     # a lot of dependencies; I cannot delete those manually
@@ -350,22 +351,18 @@ remove_subfinder() {
     rm -rf /usr/bin/subfinder
 }
 
-remove_wordlists() {
-    rm -rf /opt/wordlists
-}
-
 remove_massdns() {
+    rm -rf "${MASSDNS_PATH}"
     rm -rf /usr/bin/massdns
-    rm -rf /opt/massdns
 }
 
 remove_masscan() {
-    rm -rf /opt/masscan
+    rm -rf "${MASSCAN_PATH}"
     rm -rf /usb/bin/masscan
 }
 
 remove_dnmasscan() {
-    rm -rf /opt/dnmasscan
+    rm -rf "${DNMASSCAN_PATH}"
     rm -rf /usr/bin/dnmasscan
 }
 
@@ -378,17 +375,21 @@ remove_medusa() {
 }
 
 remove_brutespray() {
-    rm -rf /opt/brutespray
+    rm -rf "${BRUTESPRAY_PATH}"
     rm -rf /usr/bin/brutespray
 }
 
 remove_favfreak() {
-    rm -rf /opt/favfreak
+    rm -rf "${FAVFREAK_PATH}"
     rm -rf /usr/bin/favfreak
 }
 
 remove_subover() {
     rm -rf /usr/bin/subover
+}
+
+remove_wordlists() {
+    rm -rf "${WORDLISTS_PATH}"
 }
 
 remove_all() {
@@ -428,6 +429,19 @@ WORKING_DIR="$(realpath "$(dirname "${0}")")"
 
 GO_PACKAGE_PATH="/opt/go"
 EYEWITNESS_PATH="/opt/eyewitness"
+ASNLOOKUP_PATH="/opt/asnlookup"
+DOMLINK_PATH="/opt/domlink"
+GETRELATIONSHIP_PATH="/opt/getrelationship"
+SUBDOMAINIZER_PATH="/opt/subdomainizer"
+SUBSCRAPER_PATH="/opt/subscraper"
+SHOSUBGO_PATH="/opt/shosubgo"
+GITHUB_SEARCH_PATH="/opt/github-search"
+MASSDNS_PATH="/opt/massdns"
+MASSCAN_PATH="/opt/masscan"
+DNMASSCAN_PATH="/opt/dnmasscan"
+BRUTESPRAY_PATH="/opt/brutespray"
+FAVFREAK_PATH="/opt/favfreak"
+WORDLISTS_PATH="/opt/wordlists"
 
 usage() {
     echo "Usage: ${0} <install|remove> [--user <user_owning_tools>]"
